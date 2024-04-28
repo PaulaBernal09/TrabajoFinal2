154312ff4917b2855ff0a0953b884229a7a237dd
Paula Bernal <pbernal1@ibero.edu.co>
2024-04-28 01:40:01 -0500

    recuperando tiempo

--- src/config/baseDatos.js
+++ src/config/baseDatos.js
@@ -1,1 +1,22 @@
+import mongoose from "mongoose";
 
+
+
+
+const conexionMongo = async() => {
+
+await mongoose.connect(process.env.BD_URL,{})
+
+
+
+try{
+    console.log('conexión exitosa');
+} catch(error) {
+
+    console.log('Error de conexión', error.message);
+
+}
+
+}
+
+export default conexionMongo;
