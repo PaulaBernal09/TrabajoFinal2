1984ac9d039645f6ad904092e771f8a34323bdb3
Paula Bernal <pbernal1@ibero.edu.co>
2024-04-28 01:04:19 -0500

    first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper",{
+
+    sliderPerview: 1,
+    spaceBetween: 30,
+    grabCursor:true,
+    loop: true,
+    breakpoints: {
+        991: { slidesPerview: 4}
+    }
+}
+)
